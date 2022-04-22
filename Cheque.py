360eb2f422e6221d039bfc630df4d9c473391447
sumo-boi <kozhushko2006@gmail.com>
2022-04-22 18:08:56 +0500

    1

---
+++ Cheque.py
@@ -0,0 +1,11 @@
+from pyqiwip2p import QiwiP2P
+
+p2p = QiwiP2P(auth_key='eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6Ijd1YWFmdi0wMCIsInVzZXJfaWQiOiI3OTEyNDM3MTc0MyIsInNlY3JldCI6IjBhYzc1YmJlNDgwMjFlZDllNGJiNDE3YmNmNTAzZDY4ZmI0ODk4ODVkNGE0MmQ1ZGEwMDg5NGRiMzdiYzcwOTgifX0=')
+
+def generateNewBill(price:int):
+	new_bill = p2p.bill(amount=price, lifetime=20)
+	return new_bill.pay_url
+
+while True:
+	price = int(input('Введи стоимость товара\nВажно!! Стоимость товара должна быть округлённой до целого числа!!\n'))
+	print(generateNewBill(price=price))
